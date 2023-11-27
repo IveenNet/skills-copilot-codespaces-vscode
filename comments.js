172a4522538d45d219efa7751e9c0e96922221b7
Ivan David Medina Vallez <iveen98@gmail.com>
2023-11-27 16:35:35 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,32 +1,32 @@
-//Create new server-side controller called 'comments.js' and add it to the server/controllers folder. Add the following code to the new controller file:
-var mongoose = require('mongoose');
-var Comment = mongoose.model('Comment');
-var Post = mongoose.model('Post');
+// Create web server we run this file
+// node comments.js
+// http://localhost:3000/comments
+// http://localhost:3000/comments/1
+// http://localhost:3000/comments/2
 
-module.exports = {
-    create: function(req, res) {
-        Post.findOne({_id: req.params.id}, function(err, post){
-            var comment = new Comment(req.body);
-            comment._post = post._id;
-            post.comments.push(comment);
-            comment.save(function(err){
-                post.save(function(err){
-                    if(err){
-                        console.log('Error');
-                    } else {
-                        res.redirect('/');
-                    }
-                });
-            });
-        });
-    }
-}
-// The above code creates a new comment and associates it with the post from which it was created. Then it saves the comment and updates the post's comments array with the new comment. Finally, it redirects to the root route so the page can render the updated post.
-//
-// Now we need to integrate this controller into the routes. Open the server/config/routes.js file and add the following lines of code to the routes:
-app.post('/posts/:id', function(req, res){
-    comments.create(req, res);
-});
-// Now we are ready to test our application. Restart the server and go to localhost:8000. Create a new post and then create a comment for that post. If everything is working correctly, the comment should be saved and the page should refresh to show the new comment.
-//
-// We now have a fully functional MEAN application. We have the ability to create and display posts and comments, and we can even comment on comments! We will continue to build on this application in the next few assignments. In the meantime, feel free to play around with it and add your own features and functionality.
+// http://localhost:3000/comments?postId=1
+// http://localhost:3000/comments?postId=2
+
+const jsonServer = require('json-server')
+const server = jsonServer.create()
+const router = jsonServer.router('comments.json')
+const middlewares = jsonServer.defaults()
+
+server.use(middlewares)
+
+// Add custom routes before JSON Server router
+server.get('/echo', (req, res) => {
+  res.jsonp(req.query)
+})
+
+// To handle POST, PUT and PATCH you need to use a body-parser
+// You can use the one used by JSON Server
+//server.use(jsonServer.bodyParser)
+server.use(jsonServer.bodyParser)
+server.use((req, res, next) => {
+  if (req.method === 'POST') {
+    req.body.createdAt = Date.now()
+  }
+  // Continue to JSON Server router
+  next()
+})
